762626d5436e6f280f63914d592fa83889fa8bc4
wangyanqiang <wangyanqiang@sensoro.com>
2018-02-06 16:44:18 +0800

    Go 语言递归函数

---
+++ 17_Go 语言递归函数.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int = 65;
+	fmt.Printf("%d 的阶乘是 %d\n", i, Factorial(uint64(i)))
+	fmt.Println("---------------------------")
+	for i := 0; i < 65; i++ {
+		fmt.Printf("%d-%d\n", i, fibonacci(i))
+	}
+}
+
+func fibonacci(n int) int {
+	if (n < 2) {
+		return n
+	}
+	return fibonacci(n-2) + fibonacci(n-1)
+}
+
+func Factorial(n uint64) (result uint64) {
+	if (n > 0) {
+		result = n * Factorial(n-1)
+		return result
+	}
+	return 1;
+}
