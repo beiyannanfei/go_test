d6633fe59bdc0222ea1b0046a841cbf582052f57
wangyanqiang <wangyanqiang@sensoro.com>
2018-02-07 12:01:07 +0800

    Go 程序的一般结构

---
+++ geek/02_Go 程序的一般结构.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+const c = "C"
+
+var v int = 5
+
+type T struct{}
+
+func init() { // initialization of package
+	fmt.Println("... init fun ...")
+}
+
+func main() {
+	fmt.Println("... main ...")
+	var a int
+	Func1()
+	// ...
+	fmt.Println(a)
+}
+
+func (t T) Method1() {
+	//...
+	fmt.Println("... Method1 ...")
+}
+
+func Func1() { // exported function Func1
+	//...
+	fmt.Println("... Func1 ...")
+}
