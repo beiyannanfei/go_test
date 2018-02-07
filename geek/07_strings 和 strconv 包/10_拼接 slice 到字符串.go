1981dfba314d99337a0a3b53e0323aa369a576b6
wangyanqiang <wangyanqiang@sensoro.com>
2018-02-07 22:53:52 +0800

    拼接 slice 到字符串

---
+++ geek/07_strings 和 strconv 包/10_拼接 slice 到字符串.go
@@ -0,0 +1,27 @@
+//Join 用于将元素类型为 string 的 slice 使用分割符号来拼接组成一个字符串：
+//strings.Join(sl []string, sep string) string
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str := "The quick brown fox jumps over the lazy dog"
+	sl := strings.Fields(str)
+	fmt.Printf("Splitted in slice: %v\n", sl)
+	for _, val := range sl {
+		fmt.Printf("%s - ", val)
+	}
+	fmt.Println()
+	str2 := "GO1|The ABC of Go|25"
+	sl2 := strings.Split(str2, "|")
+	fmt.Printf("Splitted in slice: %v\n", sl2)
+	for _, val := range sl2 {
+		fmt.Printf("%s - ", val)
+	}
+	fmt.Println()
+	str3 := strings.Join(sl2, ";")
+	fmt.Printf("sl2 joined by ;: %s\n", str3)
+}
