f95e11337dee789b7018e7584ff8d8784a9c0ad3
wangyanqiang <wangyanqiang@sensoro.com>
2018-02-08 11:19:18 +0800

    if-else 结构

---
+++ geek/09_if-else 结构.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+var prompt = "Enter a digit, e.g. 3 " + "or %s to quit."
+
+func init() {
+	if runtime.GOOS == "windows" {
+		prompt = fmt.Sprintf(prompt, "Ctrl+Z, Enter")
+	} else { //Unix-like
+		prompt = fmt.Sprintf(prompt, "Ctrl+D")
+	}
+}
+
+func main() {
+	fmt.Println(prompt)
+	if_test()
+	else_test()
+}
+
+func if_test() {
+	bool1 := true
+	if bool1 {
+		fmt.Printf("The value is true\n")
+	} else {
+		fmt.Printf("The value is false\n")
+	}
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func isGreater(x, y int) bool {
+	if x > y {
+		return true
+	}
+	return false
+}
+
+func else_test() {
+	var first int = 10
+	var cond int
+
+	if first <= 0 {
+		fmt.Printf("first is less than or equal to 0\n")
+	} else if first > 0 && first < 5 {
+		fmt.Printf("first is between 0 and 5\n")
+	} else {
+		fmt.Printf("first is 5 or greater\n")
+	}
+	if cond = 5; cond > 10 {
+		fmt.Printf("cond is greater than 10\n")
+	} else {
+		fmt.Printf("cond is not greater than 10\n")
+	}
+}
