a6d1ae54cfe53d799a8a56646bf8ec8530ad02be
wangyanqiang <wangyanqiang@sensoro.com>
2018-02-08 16:18:21 +0800

    函数参数与返回值

---
+++ geek/03_函数/01_函数参数与返回值.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Multiply 2 * 5 * 6 = %d\n", MultiPly3Nums(2, 5, 6))
+	fmt.Println(getX2AndX3(10))
+	fmt.Println(getX2AndX3_2(100))
+	i1, _, f1 := ThreeValues()
+	fmt.Printf("The int: %d, the float: %f \n", i1, f1)
+	min, max := MinMax(78, 65)
+	fmt.Printf("Minmium is: %d, Maximum is: %d\n", min, max)
+
+	n := 0
+	reply := &n
+	Multiply(10, 5, reply)
+	fmt.Println("Multiply:", *reply) // Multiply: 50
+}
+
+func Multiply(a, b int, reply *int) { //引用传值
+	*reply = a * b
+}
+
+func MinMax(a int, b int) (min int, max int) {
+	if a < b {
+		min = a
+		max = b
+	} else { // a = b or a < b
+		min = b
+		max = a
+	}
+	return
+}
+
+func ThreeValues() (int, int, float32) {
+	return 5, 6, 7.5
+}
+
+func getX2AndX3_2(input int) (x2 int, x3 int) {
+	x2 = 2 * input
+	x3 = 3 * input
+	return //<=> return x2, x3
+}
+
+func getX2AndX3(input int) (int, int) {
+	return 2 * input, 3 * input
+}
+
+func MultiPly3Nums(a int, b int, c int) int {
+	// var product int = a * b * c
+	// return product
+	return a * b * c
+}
