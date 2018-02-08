9dfaed7adaa70cfd9f06d6a473faeb9569c14b90
wangyanqiang <wangyanqiang@sensoro.com>
2018-02-08 22:54:00 +0800

    将函数作为参数

---
+++ geek/03_函数/08_将函数作为参数.go
@@ -0,0 +1,19 @@
+//函数可以作为其它函数的参数进行传递，然后在其它函数内调用执行，一般称之为回调。
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	callback(1, Add)
+}
+
+func Add(a, b int) {
+	fmt.Printf("The sum of %d and %d is: %d\n", a, b, a+b)
+}
+
+func callback(y int, f func(int, int)) {
+	f(y, 2) // this becomes Add(1, 2)
+}
