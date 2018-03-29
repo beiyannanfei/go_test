7b041b95eb644bc0ac665d747030326b28bd863c
wyq <18810776836@163.com>
2018-03-29 13:03:25 +0800

    update

--- app/redis/client.go
+++ app/redis/client.go
@@ -3,6 +3,7 @@ package redis
 import (
 	"github.com/mediocregopher/radix.v2/redis"
 	"fmt"
+	"reflect"
 )
 
 var rediClient *redis.Client
@@ -13,5 +14,5 @@ func init() {
 
 	redisAddr := fmt.Sprintf("%s:%d", host, port)
 	fmt.Printf("client init redis adrr: %v\n", redisAddr)
-
+	fmt.Printf("redisClient's type is: %v\n", reflect.TypeOf(rediClient))
 }
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "github.com/beiyannanfei/go_test/app" //生成一个全局唯一id (放开注释可以直接使用)
+	//_ "github.com/beiyannanfei/go_test/app" //生成一个全局唯一id (放开注释可以直接使用)
 	_ "github.com/beiyannanfei/go_test/app/redis"
 )
 
