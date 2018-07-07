a6cf6df5ea0ec04ac3fc313dd9613417c51c06ea
wyq <18810776836@163.com>
2018-07-07 15:28:34 +0800

    文件上传

--- z_gin/main.go
+++ z_gin/main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"fmt"
+	"os"
+	"io"
 )
 
 func main() {
@@ -12,18 +14,25 @@ func main() {
 	//最简单路由写法	curl "127.0.0.1:8090/get/t1"
 	router.GET("/get/t1", func(c *gin.Context) {
 		fmt.Println("Hello Gin")
-		c.JSON(200, gin.H{
-			"message": "Hello Gin",
-		})
+		c.JSON(200, gin.H{"message": "Hello Gin"})
 	})
 
 	//获取路由参数 	curl "127.0.0.1:8090/get/param/bynf"
 	router.GET("/get/param/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		fmt.Printf("Hello %v\n", name)
-		c.JSON(200, gin.H{
-			"msg": "OK",
-		})
+		c.JSON(200, gin.H{"msg": "OK"})
+	})
+
+	//可选路由参数  本路由可以命中： /get/param/aaa/run  /get/param/aaa/
+	//如果没有定义路由/get/param/:name 则也可以命中 /get/param/aaa
+	//curl "127.0.0.1:8090/get/param/bynf/driver"
+	//curl "127.0.0.1:8090/get/param/bynf/"
+	router.GET("/get/param/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		fmt.Printf("%s is %s\n", name, action)
+		c.JSON(http.StatusOK, gin.H{"msg": "ok"})
 	})
 
 	//获取查询参数	curl "127.0.0.1:8090/get/query?lname=yq"
@@ -31,9 +40,7 @@ func main() {
 		fname := c.DefaultQuery("fname", "wang") //获取参数fname的值，如果没有则取默认值
 		lname := c.Query("lname")
 		fmt.Printf("Hello %v %v\n", fname, lname)
-		c.JSON(200, gin.H{
-			"msg": "OK",
-		})
+		c.JSON(200, gin.H{"msg": "OK"})
 	})
 
 	//获取post表单类型参数	curl "127.0.0.1:8090/post/form" -d "type=ABCD&msg=EFGH"
@@ -41,10 +48,19 @@ func main() {
 		mytype := c.DefaultPostForm("type", "alert")
 		msg := c.PostForm("msg")
 		fmt.Printf("type: %s, msg: %s\n", mytype, msg)
-		c.JSON(200, gin.H{
-			"msg": "ok",
-		})
+		c.JSON(200, gin.H{"msg": "ok"})
 		return
+	})
+
+	//query + post form
+	//curl "127.0.0.1:8090/post/query/form?id=1234&page=1" -d "name=bynf&age=29"
+	router.POST("/post/query/form", func(c *gin.Context) {
+		id := c.Query("id")
+		page := c.DefaultQuery("page", "0")
+		name := c.PostForm("name")
+		age := c.DefaultPostForm("age", "18")
+		fmt.Printf("id: %s, page: %s, name: %s, age: %s\n", id, page, name, age)
+		c.JSON(http.StatusOK, gin.H{"msg": "OK"})
 	})
 
 	//路由群组
@@ -121,5 +137,41 @@ func main() {
 		return
 	})
 
-	http.ListenAndServe(":8090", router)
+	//上传文件
+	//curl -X POST "127.0.0.1:8090/upload" -F "file=@/Users/wyq/workspace/lovejob/README.md" -H "Content-Type: multipart/form-data"
+	router.POST("/upload", func(c *gin.Context) {
+		file, header, err := c.Request.FormFile("file")
+		if err != nil {
+			c.String(http.StatusBadRequest, "Bad request")
+			return
+		}
+		//文件的名称
+		filename := header.Filename
+
+		fmt.Printf("file: %v\n", file)
+		fmt.Printf("header: %v\n", header)
+
+		out, err := os.Create("./t_" + filename)
+		if err != nil {
+			fmt.Printf("os create file err: %v", err.Error())
+			c.JSON(http.StatusInternalServerError, gin.H{"errmsg": err.Error()})
+			return
+		}
+
+		defer out.Close()
+
+		_, err = io.Copy(out, file)
+		if err != nil {
+			fmt.Printf("io copy err: %v", err.Error())
+			c.JSON(http.StatusInternalServerError, gin.H{"errmsg": err.Error()})
+			return
+		}
+
+		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded", filename))
+	})
+
+
+
+	//http.ListenAndServe(":8090", router)		//两种方式均可以启动服务
+	router.Run(":8090")
 }
