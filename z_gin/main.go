67197e7213d620a40eacab91e6fb78d8760a2652
wyq <18810776836@163.com>
2018-07-09 16:52:13 +0800

    update

--- z_gin/main.go
+++ z_gin/main.go
@@ -11,8 +11,9 @@ func main() {
 	r := gin.Default()
 
 	// Logging to a file.
+	//将请求日志输出到文件(不是认为输出的日志，类似：[GIN] 2018/07/09 - 14:43:53 | 200 [0m|       13.48µs |       127.0.0.1 |  GET [0m    /log/t1)
 	f, _ := os.Create("gin.log")
-	gin.DefaultWriter = io.MultiWriter(f)	//将请求日志输出到文件
+	gin.DefaultWriter = io.MultiWriter(f)
 
 	l := r.Group("/log")
 	l.Use(gin.Logger())
