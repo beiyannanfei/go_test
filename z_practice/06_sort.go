f3c1592d47a81ecf04555e1940df6d3a437034b6
wyq <18810776836@163.com>
2018-07-27 18:11:34 +0800

    切片排序

---
+++ z_practice/06_sort.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"sort"
+	"fmt"
+)
+
+func main() {
+	intList := []int{2, 4, 3, 5, 7, 6, 9, 8, 1, 0}
+	oriIntList := make([]int, len(intList), cap(intList))
+	copy(oriIntList, intList)
+	sort.Ints(intList) //升序		Float64s  Strings
+	fmt.Printf("oriIntList: %#v, intList: %#v\n", oriIntList, intList)
+	sort.Sort(sort.Reverse(sort.IntSlice(intList)))	//降序
+	fmt.Printf("oriIntList: %#v, intList: %#v\n", oriIntList, intList)
+}
