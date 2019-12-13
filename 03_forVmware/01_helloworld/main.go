8a5993248f63d097e7abb4468a9e694ad33d61c5
yanqiang.wang <wyq@yanqiangwangdeMacBook-Pro.local>
2019-12-14 06:50:03 +0800

    update

--- 03_forVmware/01_helloworld/main.go
+++ 03_forVmware/01_helloworld/main.go
@@ -1,1 +1,17 @@
-package _1_helloworld
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func show() {
+	log.Println("=============== hello world ===============")
+}
+
+func main() {
+	for {
+		show()
+		time.Sleep(time.Second * 1)
+	}
+}
